7a6a8319c4641974037224ce3dd95c0b92835f44
Simon St James <kdedevel@etotheipiplusone.com>
2013-02-04 09:49:48 +0000

    Very first baby-step - add a prototype Emscripten toolchain file (adapted from http://techbase.kde.org/Getting_Started/Build/Windows/Cross-Compiling)

---
+++ emscripten-stuff/Toolchain-emscripten.cmake
@@ -0,0 +1,29 @@
+set(KDE_PREFIX    $ENV{"EMSCRIPTEN_KDE_INSTALL"})
+set(KDE4_SRC_DIR  $ENV{"EMSCRIPTEN_KDE_SRC"})
+ 
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Emscripten)
+ 
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER clang)
+SET(CMAKE_CXX_COMPILER clang++)
+ 
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH  ${ENV}{"EMSCRIPTEN_ROOT"} ${KDE_PREFIX} )
+ 
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+ 
+# FindQt4.cmake querys qmake to get information, this doesn't work when crosscompiling
+set(QT_BINARY_DIR   ${KDE4_SRC_DIR}/qt-copy/bin)
+set(QT_LIBRARY_DIR  ${KDE_PREFIX}/lib)
+set(QT_QTCORE_LIBRARY   ${KDE_PREFIX}/lib/libQtCore4.a)
+set(QT_QTCORE_INCLUDE_DIR ${KDE_PREFIX}/include/QtCore)
+set(QT_MKSPECS_DIR  ${KDE_PREFIX}/mkspecs)
+set(QT_MOC_EXECUTABLE  ${QT_BINARY_DIR}/moc)
+set(QT_QMAKE_EXECUTABLE  ${QT_BINARY_DIR}/qmake)
+set(QT_UIC_EXECUTABLE  ${QT_BINARY_DIR}/uic)
