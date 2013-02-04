af373934c66616e56f8e7423ecff17623ac99fc2
Simon St James <kdedevel@etotheipiplusone.com>
2013-02-04 19:33:09 +0000

    No QSettings for Emscripten (yet), so cut out the QSettings stuff in KStandardDirs::addResourcesFrom_krcdirs.

--- kdecore/kernel/kstandarddirs.cpp
+++ kdecore/kernel/kstandarddirs.cpp
@@ -1677,6 +1677,7 @@ void KStandardDirs::addResourcesFrom_krcdirs()
     if (!QFile::exists(localFile))
         return;
 
+#ifndef EMSCRIPTEN
     QSettings iniFile(localFile, QSettings::IniFormat);
     iniFile.beginGroup(QString::fromLatin1("KStandardDirs"));
     const QStringList resources = iniFile.allKeys();
@@ -1689,6 +1690,9 @@ void KStandardDirs::addResourcesFrom_krcdirs()
         if(path.makeAbsolute())
             addResourceDir(key.toAscii(), path.path(), false);
     }
+#else
+    kWarning() << "Not adding any KStandardDirs resource dirs for Emscripten.";
+#endif
 }
 
 void KStandardDirs::addKDEDefaults()
