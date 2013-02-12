6e6a72d1c6c046a9c642396b68a7cd6ba76e4080
Simon St James <kdedevel@etotheipiplusone.com>
2013-02-12 09:41:06 +0000

    Begin work on an emscripten-native build of kdelibs (analogous to emscripten-native in Qt) : this is a native (x86) build that uses an SDL surface in favour of the HTML5 canvas, and is very useful for finding undefined symbols (at compile time) and for debugging (plus, it compiles much faster).

---
+++ emscripten-stuff/Toolchain-emscripten-native.cmake
@@ -0,0 +1,40 @@
+set(KDE_PREFIX    $ENV{EMSCRIPTEN_KDE_INSTALL})
+set(KDE4_SRC_DIR  $ENV{EMSCRIPTEN_KDE_SRC})
+set(EMSCRIPTEN_SYSTEM $ENV{EMSCRIPTEN_ROOT_PATH}/system/)
+ 
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Emscripten)
+SET(CMAKE_SYSTEM_PROCESSOR Emscripten)
+SET (CMAKE_CROSSCOMPILING   TRUE)
+ 
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER ccache clang)
+SET(CMAKE_CXX_COMPILER ccache clang++)
+SET(CMAKE_C_LINK_EXECUTABLE "llvm-link -o <TARGET> <LINK_FLAGS> <OBJECTS>")
+SET(CMAKE_CXX_LINK_EXECUTABLE ${CMAKE_C_LINK_EXECUTABLE})
+SET(CMAKE_CXX_CREATE_SHARED_LIBRARY ${CMAKE_C_LINK_EXECUTABLE})
+set(CMAKE_CXX_FLAGS "-emit-llvm -nostdinc++ -nostdinc -Qunused-arguments -fcolor-diagnostics -m32 -O0 -isystem${EMSCRIPTEN_SYSTEM}/include -isystem${EMSCRIPTEN_SYSTEM}/include/bsd -isystem${EMSCRIPTEN_SYSTEM}/include/libcxx -isystem${EMSCRIPTEN_SYSTEM}/include/libc -isystem${EMSCRIPTEN_SYSTEM}/include/sys -isystem${EMSCRIPTEN_SYSTEM}/include/net -isystem${EMSCRIPTEN_SYSTEM}/lib/libcxxabi/include/" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS "-emit-llvm -nostdinc -Qunused-arguments -fcolor-diagnostics -m32 -isystem${EMSCRIPTEN_SYSTEM}/include -isystem${EMSCRIPTEN_SYSTEM}/include/bsd -isystem${EMSCRIPTEN_SYSTEM}/include/libcxx -isystem${EMSCRIPTEN_SYSTEM}/include/libc -isystem${EMSCRIPTEN_SYSTEM}/include/sys " CACHE STRING "" FORCE)
+add_definitions("-DEMSCRIPTEN") 
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH  ${ENV}{EMSCRIPTEN_ROOT_PATH} ${KDE_PREFIX} )
+ 
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+ 
+# FindQt4.cmake querys qmake to get information, this doesn't work when crosscompiling
+set(QT_BINARY_DIR   /usr/bin)
+set(QT_LIBRARY_DIR  ${KDE_PREFIX}/lib)
+set(QT_HEADERS_DIR  ${KDE_PREFIX}/include)
+set(QT_QTCORE_LIBRARY   ${KDE_PREFIX}/lib/QtCore.a)
+set(QT_QTCORE_INCLUDE_DIR ${KDE_PREFIX}/include/QtCore)
+set(QT_MKSPECS_DIR  ${KDE_PREFIX}/mkspecs)
+set(QT_MOC_EXECUTABLE  ${QT_BINARY_DIR}/moc)
+set(QT_QMAKE_EXECUTABLE  ${QT_BINARY_DIR}/qmake)
+set(QT_UIC_EXECUTABLE  ${QT_BINARY_DIR}/uic)
+set(QT_DBUSXML2CPP_EXECUTABLE ${QT_BINARY_DIR}/qdbusxml2cpp)
+set(ENV{QMAKESPEC} $ENV{EMSCRIPTEN_KDE_INSTALL}/mkspecs/qws/emscripten-clang)
